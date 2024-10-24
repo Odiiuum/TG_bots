e5350883c3d12749eb0af50da54ed18fd8b8a380
Serhii <75549738+Odiiuum@users.noreply.github.com>
2024-10-24 14:39:54 +0300

    Delete test_bot directory

--- test_bot/bot.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-import logging
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters.command import Command
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
