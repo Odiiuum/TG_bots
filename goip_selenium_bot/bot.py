bc6bef686a16e01f8ea2f8434c7dc0b547ca5248
Serhii <Ukrainets0002@gmail.com>
2024-10-24 15:53:28 +0300

    Adding logger file

--- goip_selenium_bot/bot.py
+++ goip_selenium_bot/bot.py
@@ -1,20 +1,20 @@
 import asyncio
-import logging
-
 from aiogram import Bot, Dispatcher
-
 from config_reader import config
 from database import db
 from handlers import buttons, command_handlers, none_filter
+from utils.logger import setup_logger
 
-logging.basicConfig(level=logging.INFO)
-    
+# Настраиваем логгер
+logger = setup_logger("bot.log", "INFO", True)
+
 async def main():
     bot = Bot(token=config.bot_token.get_secret_value())
     dp = Dispatcher()
     
     dp.include_routers(command_handlers.router, buttons.router, none_filter.router)
     
+    logger.info("Bot has been started!")
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
     
